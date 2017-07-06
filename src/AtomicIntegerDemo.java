0cacbaa5e98664ede10cb65f960943ee3c2760a0
akash.deep <akash.deep@skillnetinc.com>
2017-07-06 17:33:00 +0530

    Updated code

--- src/AtomicIntegerDemo.java
+++ src/AtomicIntegerDemo.java
@@ -1,8 +1,31 @@
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicIntegerDemo {
+	
+	static AtomicInteger value = new AtomicInteger();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Runnable runnable = new Runnable(){
+
+			@Override
+			public void run() {
+
+				while(value.get() < 10){
+					System.out.println(Thread.currentThread().getName() + " -> " + value.incrementAndGet());
+				}
+			}			
+		};
+		
+		Thread t1 = new Thread(runnable, "t1");
+		Thread t2 = new Thread(runnable, "t2");
+		Thread t3 = new Thread(runnable, "t3");
+		Thread t4 = new Thread(runnable, "t4");
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
 
 	}
 
